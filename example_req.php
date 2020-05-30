668e339ec0bbda418481304207cbf4c700c2393e
trodevel <trodevel@gmail.com>
2020-05-30 03:09:43 +0200

    updated example

--- example_req.php
+++ example_req.php
@@ -1,10 +1,11 @@
 <?php
 
-// $Revision: 12699 $ $Date:: 2020-02-04 #$ $Author: serge $
+// $Revision: 13173 $ $Date:: 2020-05-30 #$ $Author: serge $
 
 require_once 'protocol.php';
 require_once 'response_parser.php';
 require_once 'str_helper.php';
+require_once 'request_encoder.php';
 
 $login      = 'test';
 $user_id    = 175774203;
@@ -14,31 +15,31 @@ $session_id = "b3ef4ab3-2c1c-4590-8d9c-a764eb068dbf";
 {
     $req = new \generic_protocol\AuthenticateRequest( $session_id, $login, $password );
 
-    echo "req = " . $req->to_generic_request() . "\n";
+    echo "req = " . \generic_protocol\to_generic_request( $req ) . "\n";
 }
 
 {
     $req = new \generic_protocol\AuthenticateAltRequest( $user_id, $password );
 
-    echo "req = " . $req->to_generic_request() . "\n";
+    echo "req = " . \generic_protocol\to_generic_request( $req ) . "\n";
 }
 
 {
     $req = new \generic_protocol\CloseSessionRequest( $session_id );
 
-    echo "req = " . $req->to_generic_request() . "\n";
+    echo "req = " . \generic_protocol\to_generic_request( $req ) . "\n";
 }
 
 {
     $req = new \generic_protocol\GetUserIdRequest( $session_id, $user_id );
 
-    echo "req = " . $req->to_generic_request() . "\n";
+    echo "req = " . \generic_protocol\to_generic_request( $req ) . "\n";
 }
 
 {
     $req = new \generic_protocol\GetSessionInfoRequest( $session_id, "abababab-2e2e-5647-8dc7-6575ab87ac7e" );
 
-    echo "req = " . $req->to_generic_request() . "\n";
+    echo "req = " . \generic_protocol\to_generic_request( $req ) . "\n";
 }
 
 ?>
